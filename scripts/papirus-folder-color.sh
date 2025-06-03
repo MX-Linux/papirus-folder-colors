bdf7480a9fd484a3e39b93d276cec8ab64601f8e
MelberGithub <mx-melber@tutanota.com>
2025-06-04 00:32:07 +0200

    build 25.06.01

--- scripts/papirus-folder-color.sh
+++ scripts/papirus-folder-color.sh
@@ -6,7 +6,7 @@
 #    Copyright: 2019-2022 John Crawley <john@bunsenlabs.org>
 #
 #    Modified and expanded for MX-Linux papirus-folder-colors
-#    Version=24.01.01
+#    Version=25.06.01
 #    Copyright: 2023 Melber <melber.mx.linux@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
